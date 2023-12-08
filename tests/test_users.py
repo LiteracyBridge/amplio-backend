fa86680a2b714e4a2eb446316b16819b308e2fb4
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2023-12-08 11:33:07 +0000

    test: user_roles bug fix

--- tests/test_users.py
+++ tests/test_users.py
@@ -7,22 +7,26 @@ client = TestClient(app.app)
 
 
 def test_users_list(cognito_login):
+    # TODO: add organisation id to request headers
     response = client.get(
         "/users",
         headers={"Authorization": f"Bearer {cognito_login}"},
     )
 
     assert response.status_code == 200
-    assert response.json() == {
-        "data": [
-            {
-                "id": 1,
-                "name": "Admin",
-                "permissions": [
-                    {"module": ["permission-1", "permission-2", "permission-3"]},
-                    {"module-2": ["permission-1", "permission-2", "permission-3"]},
-                    {"acm_tbloader": ["can-create-deployment", "can-update-playlist"]},
-                ],
-            }
-        ]
-    }
+    assert response.json()["data"] == [
+        {
+            "id": 1,
+            "first_name": "John",
+            "last_name": "Doe",
+            "email": "johndoe@example.com",
+            "other_names": "Walker Stones",
+        },
+        {
+            "id": 2,
+            "first_name": "Jane",
+            "last_name": "Doe",
+            "email": "doemark@example.com",
+            "other_names": "Walker Stones",
+        },
+    ]
