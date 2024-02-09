5700bdf4cf3ac83467d6f10ce19164751eb7b2da
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2024-02-09 11:08:44 +0000

    feat(uf): add endpoint to fetch sample uf messages

--- src/routes/user_feedback/uf_messages_route.py
+++ src/routes/user_feedback/uf_messages_route.py
@@ -1,9 +1,11 @@
+from itertools import count
 from typing import Optional
 
 import boto3 as boto3
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import select
 from sqlalchemy.orm import Session, subqueryload
+from sqlalchemy.sql.functions import random
 
 from models import Analysis
 from models import UserFeedbackMessage as Message
@@ -18,7 +20,7 @@ router = APIRouter()
 
 
 @router.get("/{program_id}")
-def lambda_handler(
+def get_message(
     program_id: str,
     deployment: str,
     language: str,
@@ -43,6 +45,7 @@ def lambda_handler(
                 UserFeedbackMessage.message_uuid == message_id,
                 UserFeedbackMessage.language == language,
                 UserFeedbackMessage.program_id == program_id,
+                Message.deployment_number == deployment,
             )
             .options(
                 subqueryload(UserFeedbackMessage.recipient),
@@ -80,3 +83,48 @@ def lambda_handler(
 
     data = [result] if result is not None else []
     return ApiResponse(data=data)
+
+
+@router.get("/{program_id}/samples")
+def get_message_samples(
+    program_id: str,
+    deployment: str,
+    language: str,
+    db: Session = Depends(get_db),
+):
+    if deployment is None and language is None:
+        HTTPException(
+            status_code=400,
+            detail="Missing 'language' & 'deployment' required parameters",
+        )
+
+    total_messages = (
+        db.query(UserFeedbackMessage)
+        .where(
+            UserFeedbackMessage.language == language,
+            UserFeedbackMessage.program_id == program_id,
+            Message.deployment_number == deployment,
+        )
+        .count()
+    )
+
+    # Limit results to 51% of the total messages
+    limit = int(total_messages * 0.51)
+
+    result = (
+        db.query(UserFeedbackMessage)
+        .where(
+            UserFeedbackMessage.language == language,
+            UserFeedbackMessage.program_id == program_id,
+            Message.deployment_number == deployment,
+        )
+        .options(
+            subqueryload(UserFeedbackMessage.recipient),
+            subqueryload(UserFeedbackMessage.content_metadata),
+        )
+        .order_by(random())
+        .limit(limit)
+        .all()
+    )
+
+    return ApiResponse(data=result)
