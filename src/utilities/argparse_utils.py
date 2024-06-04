e9df335bfb1641aef5ac31696f288df8e29dcf95
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2024-06-04 06:36:10 +0000

    chore: remove scripts from pyright dirs

--- src/utilities/argparse_utils.py
+++ src/utilities/argparse_utils.py
@@ -1,7 +1,7 @@
 import argparse
 from os.path import expanduser
 from pathlib import Path
-from typing import Optional
+from typing import Any, Literal, Optional
 
 
 class StorePathAction(argparse.Action):
@@ -13,35 +13,45 @@ class StorePathAction(argparse.Action):
     """
 
     @staticmethod
-    def _expand(v: str) -> Optional[Path]:
+    def _expand(v: str) -> Path | Literal[""]:
         """
         Does the work of expanding.
         :param v: A string, possibly with a leading ~ to be expanded ot user's home directory.
         :return: A Path object that encapsulates the given path. Note that there is no guarantee of
             any actual file system object at that path.
         """
-        return v and Path(expanduser(v))  # 'None' if v is None, otherwise Path(expanduser(v))
+        return v and Path(
+            expanduser(v)
+        )  # 'None' if v is None, otherwise Path(expanduser(v))
 
-    def __init__(self, option_strings, dest, default=None, **kwargs):
-        if 'glob' in kwargs:
-            self._glob = kwargs.get('glob', False)
-            del kwargs['glob']
+    def __init__(self, option_strings, dest, default: Optional[str] = None, **kwargs):
+        if "glob" in kwargs:
+            self._glob = kwargs.get("glob", False)
+            del kwargs["glob"]
         else:
             self._glob = False
-        super(StorePathAction, self).__init__(option_strings, dest, default=self._expand(default), **kwargs)
+        super(StorePathAction, self).__init__(
+            option_strings,
+            dest,
+            default=self._expand(default),  # type: ignore
+            **kwargs
+        )
 
-    def __call__(self, parser, namespace, values, option_string=None):
-        if self._glob and not isinstance(values, list): raise TypeError(
-            "The 'glob' option requires that the value is a list; did you use 'nargs'?")
+    def __call__(self, parser, namespace, values: Any, option_string=None):
+        if self._glob and not isinstance(values, list):
+            raise TypeError(
+                "The 'glob' option requires that the value is a list; did you use 'nargs'?"
+            )
         if not isinstance(values, list):
             values = self._expand(values)
         else:
             result = []
             for value in values:
-                path:Path = self._expand(value)
+                path: Path = self._expand(value)  # type: ignore
                 path_str = str(path)
-                if self._glob and ('*' in path_str or '?' in path_str):
+                if self._glob and ("*" in path_str or "?" in path_str):
                     import glob
+
                     result.extend([Path(x) for x in glob.glob(path_str)])
                     # TODO: use Path.glob() result.extend([x for x in path.glob()])
                 else:
