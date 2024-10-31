25e270b9ae2c3b48637b3ec0a602e8b4afc3497c
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2024-10-31 14:12:12 +0000

    fix(spec): file import error

--- src/programs/spec/spec.service.ts
+++ src/programs/spec/spec.service.ts
@@ -322,7 +322,7 @@ export class ProgramSpecService {
 		const {
 			rows: [general],
 			errors: errors1,
-		} = await readXlsxFile(file.buffer, {
+		} = await readXlsxFile(file.path, {
 			// @ts-ignore
 			schema: GENERAL_SCHEMA,
 			sheet: "General",
