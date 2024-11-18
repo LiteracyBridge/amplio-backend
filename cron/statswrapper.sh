dc2faa2b3935f627eebe64011fb86cfc8744f003
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2024-11-18 22:50:56 +0000

    ci(just): update stats processing

--- cron/statswrapper.sh
+++ cron/statswrapper.sh
@@ -3,7 +3,7 @@
 echo "Stats import and processing started at $(date)"
 echo "path:$PATH"
 
-just="just --justfile $basePath/justfile"
+just="just --justfile $HOME/amplio-backend/justfile"
 
 # cd ~/acm-stats/AWS-LB
 # time ./runAll.sh
