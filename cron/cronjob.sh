fa8d2fc362f92e2c1ce3e08b65ff853ad628e12f
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2024-11-18 20:06:21 +0000

    ci(cron): update cronjob.sh

--- cron/cronjob.sh
+++ cron/cronjob.sh
@@ -1,42 +1,48 @@
 #!/bin/bash
 #
 # Helper for cronjobs. Captures stdout and stderr to log and err files.
-# 
+#
 # Useful for jobs that need to run sequentially.
 
-crondir=/home/ubuntu/cron
-cronlog=$crondir/cron.log
+crondir=$(pwd)
+
+# create logs directory if it doesn't exist
+logs_dir=$crondir/logs
+if [ ! -d $logs_dir ]; then
+  mkdir $logs_dir
+fi
+
+cronlog=$logs_dir/logcron.log
 jobtime=$(date +%Y%m%d-%H%M%S)
 
-echo "Running cron job at $(date) in $(pwd)" > $cronlog 
-cd /home/ubuntu
+echo "Running cron job at $(date) in $(pwd)" >$cronlog
+cd /home/$USER
 source ./.profile
-export PATH=/home/ubuntu:/home/ubuntu/bin:${PATH}:/snap/bin
+export PATH=/home/$USER:/home/$USER/bin:${PATH}:/snap/bin
 echo "PATH=${PATH}"
 
 function doTask() {
-    # 'name' for convenience
-    name=$1 
-    # set up log & err files, convenience link to latest log
-    logfile=$crondir/${jobtime}-${name}.log
-    errfile=$crondir/${jobtime}-${name}.err
-    touch $logfile
-    rm $crondir/${name}.log
-    ln -s $logfile $crondir/${name}.log
-    rm $crondir/${name}.err
-    ln -s $errfile $crondir/${name}.err
+  # 'name' for convenience
+  name=$1
+  # set up log & err files, convenience link to latest log
+  logfile=$logs_dir/${jobtime}-${name}.log
+  errfile=$logs_dir/${jobtime}-${name}.err
+  touch $logfile
+  rm $logs_dir/${name}.log
+  ln -s $logfile $logs_dir/${name}.log
+  rm $logs_dir/${name}.err
+  ln -s $errfile $logs_dir/${name}.err
 
-    echo "Running ${name} job, logging to $logfile" >>$cronlog
-    $crondir/${name}wrapper.sh >>$logfile 2>$errfile
+  echo "Running ${name} job, logging to $logfile" >>$cronlog
+  $crondir/${name}wrapper.sh >>$logfile 2>$errfile
 
-    # delete size-zero error logs
-    if [ ! -s $errfile ]; then
-      rm $errfile
-      rm $crondir/${name}.err
-    fi
+  # delete size-zero error logs
+  if [ ! -s $errfile ]; then
+    rm $errfile
+    rm $logs_dir/${name}.err
+  fi
 }
 
 #doTask dropbox
 
 doTask stats
-
