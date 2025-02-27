926dfdc6b4d285fca9dd169a9750d73585bad330
Titus Ayamga <www.ayamgatitus@gmail.com>
2025-02-27 08:59:23 +0000

    fix/remove invalid chars from message and playlist titles

--- src/programs/spec/spec.service.ts
+++ src/programs/spec/spec.service.ts
@@ -126,6 +126,10 @@ export class ProgramSpecService {
 			for (const d of dto.deployments) {
 				// Handle playlists data
 				for (const pl of d.playlists) {
+
+					// remove  invalid characters from playlists titles
+					pl.title = this.sanitazeString(pl.title);
+
 					const deployment = allDeployments.find(
 						(d) =>
 							d._id === pl.deployment_id ||
@@ -154,6 +158,10 @@ export class ProgramSpecService {
 					// handle playlist messages
 					// const existingMessageIds = new Set(pl.messages.flatMap((m) => m._id));
 					for (const m of pl.messages) {
+
+						// remove invalid characters from the message title
+						m.title = this.sanitazeString(m.title);
+
 						m.playlist_id = resp.id;
 						m.program_id = project.code;
 						m.default_category_code =
@@ -577,5 +585,11 @@ export class ProgramSpecService {
 		general.project_id ??= program.project_id;
 		general.languages = languages;
 		await manager.upsert(Program, general, ["program_id"]);
+	}
+
+	private sanitazeString(input: string) {
+		const invalidChars = /[\\/:*?<>_|"']/g;
+		// Replace invalid characters with a space
+		return input.replace(invalidChars, ' ');
 	}
 }
