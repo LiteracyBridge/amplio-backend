76fc8dc53b19fa1669433ba87475db6a5cbf7afe
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2025-03-05 09:28:03 +0000

    ci: update blocker script

--- scripts/port-scanner-blocker/blocker.sh
+++ scripts/port-scanner-blocker/blocker.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 IFTOP_LOG_FILE="/tmp/iftop.log"
+IFTOP_TEMP_LOG_FILE="/tmp/tmp-iftop.log"
 OUTPUT_FILE="/tmp/block-ips.sh"
 
-sudo iftop -PFG -t -n -s 3 -L 200 > $IFTOP_LOG_FILE
+sudo iftop -PFG -t -n -s 3 -L 200 > $IFTOP_TEMP_LOG_FILE
+
+awk '{ print $1, $NF }' $IFTOP_TEMP_LOG_FILE > $IFTOP_LOG_FILE
 
 python ift_logs_reader.py
 
