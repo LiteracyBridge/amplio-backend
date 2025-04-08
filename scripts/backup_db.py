f5b5546cd89e8dd29c071c95677afa085d4750bf
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2025-04-08 07:36:46 +0000

    fix: db backup fix

--- scripts/backup_db.py
+++ scripts/backup_db.py
@@ -21,7 +21,7 @@ def main():
     schema_file = f"{dest}/{config.db_name}_backup_{timestamp}.schema"
 
     # Create a data backup of the database
-    dump_command = f"pg_dump --host {config.db_host} --port {config.db_port} --username {config.db_user} --schema-only --verbose --file {schema_file} {config.db_name}"
+    dump_command = f"pg_dump --host {config.db_host} --port {config.db_port} --username {config.db_user} --schema-only --no-privileges --no-privileges --verbose --file {schema_file} {config.db_name}"
     os.environ["PGPASSWORD"] = config.db_password
 
     subprocess.run(dump_command, shell=True, check=True)
@@ -33,7 +33,7 @@ def main():
 
     # Create a schema of the database
     data_file = f"{dest}/{config.db_name}_backup_{timestamp}.data"
-    dump_command = f"pg_dump --host {config.db_host} --port {config.db_port} --username {config.db_user} --data-only --large-objects --verbose --file {data_file} {config.db_name}"
+    dump_command = f"pg_dump --host {config.db_host} --port {config.db_port} --username {config.db_user} --data-only --no-privileges --no-privileges --large-objects --verbose --file {data_file} {config.db_name}"
     os.environ["PGPASSWORD"] = config.db_password
     subprocess.run(dump_command, shell=True, check=True)
 
