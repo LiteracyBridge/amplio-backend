04d371986ff700d7e7ee7a4df0825082e8ec928f
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2025-09-30 22:10:37 +0000

    feat: add public library endpoint

--- src/companion/companion.controller.ts
+++ src/companion/companion.controller.ts
@@ -9,6 +9,7 @@ import {
 	UploadedFile,
 	UseInterceptors,
 } from "@nestjs/common";
+import { In } from "typeorm";
 import { SkipJwtAuth } from "src/decorators/skip-jwt-auth.decorator";
 import { CompanionAppService } from "./companion.service";
 import { ApiResponse } from "src/utilities/api_response";
@@ -16,6 +17,7 @@ import { Response } from "express";
 import { createReadStream } from "node:fs";
 import { CompanionStatisticsDto, RecipientDto } from "./companion.dto";
 import { FileInterceptor } from "@nestjs/platform-express";
+import { DeploymentMetadata } from "src/entities/deployment_metadata.entity";
 
 // TODO: generate unique api key on verification, required in subsequent requests
 @Controller("companion")
@@ -76,6 +78,25 @@ export class CompanionAppController {
 	async userFeedback(@UploadedFile() file: Express.Multer.File) {
 		return ApiResponse.Success({
 			data: { saved: await this.service.saveUserFeedback(file) },
+		});
+	}
+
+	@SkipJwtAuth()
+	@Post("library")
+	async publicLibrary() {
+		const packageIds: { id: string }[] = await DeploymentMetadata.query(`
+      SELECT DISTINCT ON (meta.project_id) meta.id
+      FROM deployment_metadata meta
+      WHERE meta.published = true AND platform = 'CompanionApp'
+      ORDER BY meta.project_id, meta.revision DESC;
+    `);
+
+		const metadata = DeploymentMetadata.find({
+			where: { id: In(packageIds.map((p) => p.id)) },
+			relations: { project: true },
+		});
+		return ApiResponse.Success({
+			data: metadata,
 		});
 	}
 }
