b633bbb5dbd4c71ddc9bd59e88f9e37ba02c4441
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2026-04-06 10:25:00 +0000

    wip: survey file parsing

--- src/tb-analytics/custom-survey-report.service.ts
+++ src/tb-analytics/custom-survey-report.service.ts
@@ -1,5 +1,11 @@
 import { BadRequestException, Injectable } from "@nestjs/common";
+import { tmpdir } from "node:os";
 import { DataSource } from "typeorm";
+import fs from "node:fs";
+import appConfig from "src/app.config";
+import { GetObjectCommand } from "@aws-sdk/client-s3";
+import { s3Client } from "src/utilities";
+// import { parse, stringify } from "yaml";
 
 @Injectable()
 export class CustomSurveyReportService {
@@ -22,7 +28,7 @@ export class CustomSurveyReportService {
 				dto.survey,
 			]);
 
-			const report = await queryRunner.query(`
+			let report: any[] = await queryRunner.query(`
         SELECT
           r.communityname AS "Community",
           r.groupname AS "Group",
@@ -35,7 +41,22 @@ export class CustomSurveyReportService {
         INNER JOIN recipients r ON r.recipientid = ss.recipientid;
       `);
 
-			console.log(report);
+			if (report.length > 0) {
+				const parts = await this.parseSurveyContents(dto);
+
+				// Parse the generated report and replace q1, q2... with the corresponding message titles
+				report = report.map((row) => {
+					for (const p of parts) {
+						row[p.message] = row[p.question]; // Assign value to new key
+						delete row[p.question]; // Remove the old key
+					}
+
+          delete row.survey_uuid;
+					return row;
+				});
+			}
+
+			// console.log(report);
 
 			await queryRunner.rollbackTransaction();
 
@@ -45,5 +66,53 @@ export class CustomSurveyReportService {
 		} finally {
 			await queryRunner.release();
 		}
+	}
+
+	async parseSurveyContents(dto: { programid: string; survey: string }) {
+		// Download survey file and extract message titles associated with each question
+		const destinationDir = `${tmpdir()}/custom-surveys`;
+		if (!fs.existsSync(destinationDir)) {
+			fs.mkdirSync(destinationDir);
+		}
+
+		const filePath = `${destinationDir}/${dto.programid}-${dto.survey}.survey`;
+
+		if (!fs.existsSync(filePath)) {
+			const command = new GetObjectCommand({
+				Bucket: appConfig().buckets.content,
+				Key: `${dto.programid}/programspec/SatisfactionSurvey.survey`,
+			});
+			const response = await s3Client().send(command);
+
+			const stream = response.Body as NodeJS.ReadableStream;
+			const writeStream = fs.createWriteStream(filePath);
+			await new Promise((resolve, reject) => {
+				stream.pipe(writeStream);
+				stream.on("end", resolve);
+				stream.on("error", reject);
+			});
+		}
+
+		// Parse survey yaml file
+		// const yaml = await parse(filePath);
+		const yaml = fs.readFileSync(filePath, { encoding: "utf-8" });
+
+		const regex = new RegExp(
+			"(?<question>q\\d{1,})" + // Question number -> q1, q2, ..., qn
+				"(?:\\:[\\W\\s]+Prompt\\:[\\s]+)" + // Skip "Prompt:"
+				"(?<message>[\\w\\s]+)\\r?\\n", // message title
+			"gmi",
+		);
+
+		const parts: { question: string; message: string }[] = [];
+
+		while (true) {
+			const match = regex.exec(yaml)!;
+			if (match == null) break;
+
+			parts.push(match.groups as { question: string; message: string });
+		}
+
+		return parts;
 	}
 }
